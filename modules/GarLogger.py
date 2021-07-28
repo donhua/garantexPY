5ab4a4f6d20a2e47529da16ad1bb2c099378dc5b
donhua <suvorov.mo@yandex.ru>
2021-07-28 17:24:47 +0300

    Рефактор модуля GarLogger.py. Добавлены прототипы методов для логгирования.

---
+++ modules/GarLogger.py
@@ -0,0 +1,23 @@
+import requests
+
+
+class GarLogger:
+    """Класс логирует и бекапит логи курсов, ставок, лотов"""
+
+    def __init__(self):
+        pass
+
+    def first_initial(self):
+        """Проверка наличия логов или создание новых"""
+        pass
+
+    def beckup(self):
+        """резервное копирование логов"""
+        pass
+
+    def writer(self):
+        """Запись массива данных в логи xml"""
+
+
+if __name__ == '__main__':
+    print("Модуль логирования.")
