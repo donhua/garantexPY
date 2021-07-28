610bf9f08fed885ceae83dd572acc907e94e8639
donhua <suvorov.mo@yandex.ru>
2021-07-28 17:32:29 +0300

    Рефакторинг.

---
+++ modules/GarBotTele.py
@@ -0,0 +1,5 @@
+
+class GarBotTele:
+    """телеграмм бот для торговли"""
+    def __init__(self):
+        pass
---
+++ modules/GarantexIo.py
@@ -0,0 +1,7 @@
+
+
+class GarantexIo:
+    """Методы API Garantex.io"""
+
+    def __init__(self):
+        pass
