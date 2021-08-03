f90bebbde7f9cf14cb62d43ad152b5f7d13ff088
donhua <suvorov.mo@yandex.ru>
2021-08-03 17:23:12 +0300

    настройки бд

--- modules/Gardb.py
+++ modules/Gardb.py
@@ -1,37 +1,38 @@
 import sqlite3 as sql
 
-con = sql.connect('my-test.db')
-with con:
-    cur = con.cursor()
-    con.execute("""
-        CREATE TABLE USER (
-            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-            name TEXT,
-            age INTEGER
-        );
-    """)
-    con.commit()
-    cur.close("CREATE TABLE IF NOT EXISTS `test` ('id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, `name` STRING, `surname` STRING)")
-    cur.close("CREATE TABLE IF NOT EXISTS `cur` ('id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, `cur` STRING")
-
 
 class Gardb:
 
     def __init__(self, namedb: str = 'gdb'):
-        self.namedb
+        self.namedb = namedb
         self.db = None
 
     def Connectdb(self):
         self.db = sql.connect(self.namedb)
 
-    def Closedb(self):
-        self.db.c
 
     def Createdb(self):
         self.db = sql.connect(self.namedb)
+        with self.db:
+            dbw = self.db.cursor()
+            dbw.execute("CREATE TABLE IF NOT EXISTS `test` ('id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, `name` STRING, `surname` STRING)")
+            dbw.execute("CREATE TABLE IF NOT EXISTS `curi` ('id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, `cur` STRING)")
+            dbw.execute("SELECT name FROM sqlite_master WHERE type='table';")
+            print(dbw.fetchall())
+            self.db.commit()
+
+
+    def Deletedb(self):
+        self.db = sql.connect(self.namedb)
         dbw = self.db.cursor()
-        dbw.close("CREATE TABLE IF NOT EXISTS `test` ('id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, `name` STRING, `surname` STRING)")
-        dbw.close("CREATE TABLE IF NOT EXISTS `cur` ('id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, `cur` STRING")
-        self.db.commit()
-        self.db.colose()
+        pass
+
+def mee():
+    n = Gardb()
+    n.Createdb()
+
+
+if __name__ == '__main__':
+    mee()
+
 
