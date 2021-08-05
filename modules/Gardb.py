e539e3f24627c4ae514bd611ed6ca93c4eb40de4
donhua <suvorov.mo@yandex.ru>
2021-08-05 15:01:23 +0300

    Создал стуктуру бд. Логирование событий бд. Настроен механизм бекапа.

--- modules/Gardb.py
+++ modules/Gardb.py
@@ -1,37 +1,85 @@
 import sqlite3 as sql
+import datetime
+import logging
 
 
 class Gardb:
 
-    def __init__(self, namedb: str = 'gdb.db'):
-        self.namedb = namedb
+    def __init__(self, namedb1: str = 'gdb.db'):
+        self.namedb = namedb1
         self.db = None
-        self.sq = ['create table if not exists student(roll_no integer PRIMARY KEY,first_name text,\
-                    last_name text, class text, stream text,address text)',]
+        self.table_name = ['curensy', 'curensy_type', 'lot', 'kash']
+        self.sq = [
+                    f'''CREATE TABLE IF NOT EXISTS {self.table_name[0]}(
+                    id INT PRIMARY KEY,
+                    kur TEXT(5)
+                    );''',
+                    f'''CREATE TABLE IF NOT EXISTS {self.table_name[1]}(
+                    id INT PRIMARY KEY,
+                    type TEXT(5)
+                    );''',
+                    f'''CREATE TABLE IF NOT EXISTS {self.table_name[2]}(
+                    id INT PRIMARY KEY,
+                    lot_Date TIMESTAMP,
+                    lot_fiat REAL,
+                    lot_coin REAL,
+                    coin_type INT,
+                    exchange real
+                    );''',
+                    f'''CREATE TABLE IF NOT EXISTS {self.table_name[3]}(
+                    id INT PRIMARY KEY,
+                    kash_Date TIMESTAMP,
+                    kash_fiat REAL
+                    );''',
+                   ]
+        logging.basicConfig(
+                            filename='../log/app.log',
+                            level=logging.INFO,
+                            format='%(asctime)s-%(levelname)s-%(message)s',
+                            )
 
     def Connectdb(self):
         try:
             self.db = sql.connect(self.namedb)
-            print(f"Статус соединения с базой {self.namedb} - УСПЕШНО!")
+            logging.info(f"Статус соединения с базой {self.namedb} - УСПЕШНО!")
+            for i in self.sq:
+                self.Create_table(i, self.db.cursor())
+            self.db.commit()
         except sql.Error:
-            print(sql.Error)
+            logging.error(sql.Error)
         finally:
-            print('ok')
+            self.db.close()
+            logging.info(f'Закрытие базы {self.namedb} - успешно!')
 
-    def Create_table(self):
-        cursor_object = self.db.cursor()
-        cursor_object.execute(self.sq[0])
-        self.db.commit()
-        print('yes')
-        self.db.close()
+    def Create_table(self, sql_request, cursor_object):
+        cursor_object.execute(sql_request)
+        logging.info(f'Создание таблицы {self.table_name[0]} - успешно!')
 
     def Deletedb(self):
         pass
 
+    def Gar_backup(self):
+        try:
+            t = datetime.datetime.now()
+            self.db = sql.connect(self.namedb)
+            backup_db_name = str(t) +"_" + self.namedb[:(len(self.namedb)-3)] + '_backup.db'
+            backup_con = sql.connect('../backup/'+backup_db_name)
+            with backup_con:
+                self.db.backup(backup_con, pages=3, progress=None)
+            logging.info(f"Бекап {self.namedb} - успешно!")
+        except sql.Error as error:
+            logging.error(f"Ошибка бекапа {self.namedb}: " + str(error))
+        finally:
+            if backup_con:
+                backup_con.close()
+                self.db.close()
+                logging.info(f'Закрытие базы {self.namedb} и {backup_db_name}- успешно!')
+
+
 def mee():
     n = Gardb()
     n.Connectdb()
-    n.Create_table()
+    n.Gar_backup()
 
 
 if __name__ == '__main__':
