9b9a219db72ce8b6dc336c006c920463b123474d
Donhua <suvorov.mo@yandex.ru>
2021-10-20 17:59:31 +0300

    новый вариант калькулятора

---
+++ gar_modul/CalculateFin_1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+from statistics import mean
+
+class CalculateFin1:
+    """Финансовый калькулятор"""
+
+    def sum_invests(arr_invests: list = []):
+        '''принимает список инвестиций, возвращает сумму'''
+        return sum(arr_invests)
+
+    def mean_kurs(arr_kurs):
+        '''принимает список значений, возвращает среднее'''
+        return round(mean(arr_kurs), 2)
+
+    def signal_seil(kurs_in_time, kurs_meam, persent_marga):
+        delta = kurs_in_time-kurs_meam
+        persent_in_time = round(delta * 100 / kurs_meam, 2)
+        if persent_in_time >= persent_marga:
+            return ['', persent_in_time]
+        else:
+            return [persent_in_time, '']
