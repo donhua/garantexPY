f4d37a5d292615d6d32b13c074a9f2ba37ae5894
Donhua <suvorov.mo@yandex.ru>
2021-10-22 17:52:54 +0300

    Переработан GUI

--- gui.py
+++ gui.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python3
 
 import PySimpleGUI as sg
-import time
-exit()
+from gar_modul import GarantexIo, CalculateFin_1
+
+gi = GarantexIo.GarantexIo()
+
+def thistime():
+    named_tuple = time.localtime() # получить struct_time
+    time_string = time.strftime("%m/%d/%Y, %H:%M:%S", named_tuple)
+    return time_string
+ss = 1
 layout = [[sg.Output(size=(60,10))],
-          [sg.Button('Go'), sg.Button('Exit')]  ]
-
-
-
+          [sg.Button('Go'), sg.Button('Pause')]  ]
 
 window = sg.Window('Window Title', layout)
-while True:             # Event Loop
-    event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Exit':
-        break
-    if event == 'Go':
-        while True:
-            print(f"This time: {time.gmtime()}")
-            print("================")
 
+while True:             # Event Loop
+    event, values = window.read(timeout=1000)
+    if event == sg.WIN_CLOSED:
+        exit()
+        break
+
+    if event == 'Pause':
+        quit()
+  
+    print(thistime())
 window.close()
